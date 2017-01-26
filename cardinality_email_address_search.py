1b549d952237c3872d52fa1f3e27ca8c1b2e9330
Chris Garcia <chrisgarcia001@gmail.com>
2017-01-25 20:37:41 -0500

    Adding debug output

--- cardinality_email_address_search.py
+++ cardinality_email_address_search.py
@@ -61,6 +61,8 @@ criteria_f = None
 try:
 	eval_f = lambda x: csv.standard_eval_input(x, sep=':')  
 	params = cmd.read_params(sys.argv, mainfile_suffix=THIS_FILENAME, input_evaluator_f=eval_f)
+	print('----- PARAMS -----')
+	print(params)
 	criteria_f = build_criteria_f(params)
 except:
 	print_usage()
